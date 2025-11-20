dd51904be27a300229de253e3a9f83c90666ff59
Dorian Clay <dorianclay@cmu.edu>
2025-11-19 21:19:13 -0500

    Copy sglang guide

---
+++ baseline/baseline_server.py
@@ -0,0 +1,11 @@
+from sglang.test.doc_patch import launch_server_cmd
+from sglang.utils import wait_for_server, print_highlight, terminate_process
+
+server_process, prot = launch_server_cmd(
+    """
+python3 -m sglang.launch_server --model-path qwen/qwen2.5-0.5b-instruct \
+ --host 0.0.0.0 --log-level warning
+"""
+)
+
+wait_for_server(f"http://localhost:{port}")
